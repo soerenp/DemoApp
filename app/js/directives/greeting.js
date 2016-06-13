088271a6bae9a3469a033c740c47dd37084c8f9d
SoerenP <soerenp89@gmail.com>
2016-06-13 11:28:42 +0200

    complete compile + jquery in directives tut

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -4,12 +4,36 @@ eventsApp.directive('greeting', function() {
   return {
     restrict: 'E',
     replace: true,
-    template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
+    transclude: true,
+    template: "<div><button class='btn' ng-click='sayHello()'>Say Hello</button><div ng-transclude></div></div>",
     controller: function($scope) {
+      var greetings = ['Hello'];
       $scope.sayHello = function() {
-        alert("Hello!");
+        alert(greetings.join());
+      }
+      this.addGreeting = function(greeting) {
+        greetings.push(greeting);
       }
 
     }
   };
+})
+.directive('finnish', function() {
+  return {
+    restrict: 'A',
+    require: '^greeting',
+    link: function(scope, element, attrs, controller) {
+      controller.addGreeting('hei');
+    }
+
+  }
+})
+.directive('hindi', function() {
+  return {
+    restrict: 'A',
+    require: '^greeting',
+    link: function(scope, element, attrs, controller) {
+      controller.addGreeting('namasthe');
+    }
+  }
 });
---
+++ app/js/directives/repeatx.js
@@ -0,0 +1,17 @@
+eventsApp.directive('repeatX', function() {
+  return {
+    compile: function(element, attrs) {
+      for (var i=0; i < Number(attrs.repeatX)-1; i++) {
+        element.after(element.clone().attr('repeat-x',0));
+      }
+      return function(scope, element, attrs, controller) {
+        attrs.$observe('text', function(newValue) {
+          if(newValue === 'Hello World') {
+            element.css('color', 'red');
+          }
+        });
+      }
+    }
+
+  };
+});
